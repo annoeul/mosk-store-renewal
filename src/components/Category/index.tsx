becbda354605686f94057f7317fb86bac67b1d29
annoeul <shdmf1137@gmail.com>
2023-11-27 18:59:17 +0900

    design: 카테고리 디자인 중

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -5,20 +5,22 @@ import { useGetDatasQuery } from "../../apis/getData"
 function Category() {
   const { data, isLoading, isError } = useGetDatasQuery(1)
 
-  const onClick = (id) => {
+  const onClick = (id: number) => {
     console.log(id)
   }
+
   if (isLoading) {
     return <>로딩중</>
   }
+
   return (
     <S.CategoryWrapper>
       Category
       {data.data &&
         data.data.map((item) => (
-          <div key={item.id}>
+          <S.CategoryBtnWrapper key={item.id}>
             <S.CategoryBtn onClick={() => onClick(item.id)}>{item.name}</S.CategoryBtn>
-          </div>
+          </S.CategoryBtnWrapper>
         ))}
     </S.CategoryWrapper>
   )
--- src/components/Category/style.ts
+++ src/components/Category/style.ts
@@ -3,15 +3,22 @@ import { styled } from "styled-components"
 export const CategoryWrapper = styled.div`
   width: 20%;
   height: 100vh;
-  border: 1px solid black;
+  /* border-right: 1px solid black; */
+  box-shadow: rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px;
+  padding: 20px;
 `
 
-export const CategoryBtn = styled.div`
+export const CategoryBtnWrapper = styled.div`
+  width: 100%;
+`
+
+export const CategoryBtn = styled.button`
   text-align: center;
   width: 100%;
   height: 3rem;
   margin: 10px 0;
-  border-bottom: 1px solid black;
-  background-color: none;
+  border: 0.1px solid black;
+  border-radius: 10px;
+  background-color: white;
   cursor: pointer;
 `
