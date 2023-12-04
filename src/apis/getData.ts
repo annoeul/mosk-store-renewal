bd3d931d6aac5b6565cd7bd2a7794f37cea060b0
annoeul <shdmf1137@gmail.com>
2023-12-04 18:15:36 +0900

    feat : 카테고리 추가 기능

--- src/apis/getData.ts
+++ src/apis/getData.ts
@@ -1,15 +1,31 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { getCookie } from "../utils/cookies"
 
 export const getData = createApi({
   reducerPath: "getData",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_SERVER_URL,
+    prepareHeaders: (headers) => {
+      const token = getCookie("accessToken")
+      headers.set("Authorization", `Bearer ${token}`)
+      return headers
+    },
   }),
   endpoints: (builder) => ({
     getDatas: builder.query({
       query: (storeId) => `/public/categories/all/${storeId}`,
     }),
+    createCategory: builder.mutation({
+      query: ({ data }) => ({
+        url: "/categories",
+        method: "POST",
+        body: JSON.stringify(data), // 데이터를 JSON 문자열로 변환
+        headers: {
+          "Content-Type": "application/json; charset=utf-8", // Content-Type 설정
+        },
+      }),
+    }),
   }),
 })
 
-export const { useGetDatasQuery } = getData
+export const { useGetDatasQuery, useCreateCategoryMutation } = getData
--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,18 +1,36 @@
 import React, { useEffect, useState } from "react"
 import * as S from "./style"
-import { useGetDatasQuery } from "../../apis/getData"
+import { useGetDatasQuery, useCreateCategoryMutation } from "../../apis/getData"
 import { useDispatch } from "react-redux"
 import { setCategory } from "../../store/slices/selectItem"
-import { Container } from "@mui/material"
+import { Container, TextField, Button } from "@mui/material"
 
 function Category({ storeId }) {
   const { data, isLoading, isError } = useGetDatasQuery(storeId)
   const dispatch = useDispatch()
   const [selectedCategory, setSelectedCategory] = useState(null)
+  const [isCreatingCategory, setIsCreatingCategory] = useState(false)
+  const [newCategory, setNewCategoryName] = useState("")
+  const [createCategoryMutation] = useCreateCategoryMutation()
 
   const handleCategoryClick = (categoryId) => {
     dispatch(setCategory(categoryId))
     setSelectedCategory(categoryId)
+  }
+
+  const handleCreateCategoryClick = () => {
+    setIsCreatingCategory(true)
+  }
+
+  const onChange = (e) => {
+    setNewCategoryName(e.target.value)
+    console.log(newCategory)
+  }
+
+  const handleCreateCategory = () => {
+    createCategoryMutation({
+      data: newCategory,
+    })
   }
 
   if (isLoading) {
@@ -21,7 +39,17 @@ function Category({ storeId }) {
 
   return (
     <S.CategoryWrapper>
-      <S.CategoryBtn>카테고리 생성</S.CategoryBtn>
+      {isCreatingCategory ? (
+        <>
+          <TextField label="카테고리 이름" variant="standard" fullWidth value={newCategory} onChange={onChange} />
+          <Button variant="contained" onClick={handleCreateCategory}>
+            확인
+          </Button>
+        </>
+      ) : (
+        <S.CategoryBtn onClick={handleCreateCategoryClick}>카테고리 생성</S.CategoryBtn>
+      )}
+
       {data.data &&
         data.data.map((item) => (
           <S.CategoryNameWrapper
