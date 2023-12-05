298a0f36420e6f9ef4d28a059ce8ec2bec9cdb9e
annoeul <shdmf1137@gmail.com>
2023-12-05 15:09:16 +0900

    feat: cookie 세팅하는 함수

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,10 +1,10 @@
 import { Cookies } from "react-cookie"
 
-const cookies = new Cookies() // 쿠키 객체를 생성하고,
+const cookies = new Cookies()
 
 export const getCookie = (name) => {
   try {
-    return cookies.get(name) // get 을 하면 name 을 기준으로 쿠키 값을 가져옵니다.
+    return cookies.get(name)
   } catch (error) {
     console.error(error)
   }
@@ -12,7 +12,7 @@ export const getCookie = (name) => {
 
 export const setCookie = (name, value, option) => {
   try {
-    cookies.set(name, value, { ...option }) // set 을 하면 name = value 형태로 쿠키를 설정할 수 있습니다.
+    cookies.set(name, value, { ...option })
   } catch (error) {
     console.error(error)
   }
@@ -20,7 +20,7 @@ export const setCookie = (name, value, option) => {
 
 export const removeCookie = (name, option) => {
   try {
-    cookies.remove(name, { ...option }) // remove 하면 해당 name 쿠키를 삭제합니다.
+    cookies.remove(name, { ...option })
   } catch (error) {
     console.error(error)
   }
